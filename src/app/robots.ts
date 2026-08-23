a80566cf022cb7cfe7ea88b195b897c0d11586b2
feet-code <feetcodee@gmail.com>
2026-08-23 14:51:11 -0700

    Fix static export for robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from "next";
 
+// robots.txt is generated entirely at build time for static export.
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   const base = (process.env.SITE_URL || "http://localhost:3000").replace(/\/$/, "");
   return {
